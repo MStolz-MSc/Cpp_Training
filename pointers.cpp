58f351d63b9122fea77502b42061ad5df211725f
MStolz <marius37@online.de>
2021-01-17 00:37:14 +0100

    Added files

---
+++ pointers.cpp
@@ -0,0 +1,21 @@
+// pointers.cpp
+
+// created by MStolz-MSc
+
+/*
+Simple script using pointers.
+Pointers can make some things easier, help improve program's efficiency
+and allow to handle (nearly) unlimited amounts of data.
+You do not need to know how much memory you need, when writing the program.
+*/
+
+// HEADER
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+}
